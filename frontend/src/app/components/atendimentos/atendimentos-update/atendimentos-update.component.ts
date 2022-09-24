cb409b8cc6fbce39d60a000a4f18cade58aaa9c2
davidwanderley <davidgomeswanderley@gmail.com>
2022-09-24 17:16:23 -0300

    CRUD - Update

--- frontend/src/app/components/atendimentos/atendimentos-update/atendimentos-update.component.ts
+++ frontend/src/app/components/atendimentos/atendimentos-update/atendimentos-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Atendimento } from '../atendimentos.model';
+import { AtendimentosService } from '../atendimentos.service';
 
 @Component({
   selector: 'app-atendimentos-update',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AtendimentosUpdateComponent implements OnInit {
 
-  constructor() { }
+  atendimento!: Atendimento;
+
+  constructor(private atendimentoService: AtendimentosService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    const d2 = Number(id);
+    this.atendimentoService.readById(d2).subscribe(atendimento => {
+      this.atendimento = atendimento;
+    });
+  }
+
+  updateAtendimento(): void {
+    this.atendimentoService.update(this.atendimento).subscribe(()=>{
+      this.atendimentoService.showMessage('Atendimento atualizado com sucesso');
+      this.router.navigate(['/atendimento']);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/atendimento'])
   }
 
 }
